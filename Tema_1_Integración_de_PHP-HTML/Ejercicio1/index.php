22da2f4975afa86c1cfb047aa029037c8cf633a1
Alberto Ortega Padilla <albertoortega576@gmail.com>
2016-09-27 17:16:05 +0200

    actualizo

--- Tema_1_Integración_de_PHP-HTML/Ejercicio1/index.php
+++ Tema_1_Integración_de_PHP-HTML/Ejercicio1/index.php
@@ -7,16 +7,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Ejercicio 1 PHP</title>
+        <title>Ejercicios de PHP</title>
     </head>
     <body>
-        
+
         <h1>TEMA 1</h1>
-        
+
         <p><b>Ejercicio 1: </b>Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP.</p>
-        
+
         <br><br>
-        
+
         <?php
             echo "Alberto";
         ?>
